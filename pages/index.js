e5ed9be8b7a82e00ecfb65a18b6ad038136ee82c
theskillwithin <austinppeterson@gmail.com>
2019-07-25 21:19:02 -0600

    create custom server

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,37 @@
 import css from "./styles.css";
 
-const Home = () => (
-  <main className={css.container}>
-    <header className={css.header}>
-      <h1>♥ VSCode Extentions Poll ♥</h1>
-      <h2>
-        visit{" "}
-        <a
-          href="https://marketplace.visualstudio.com/vscode"
-          target="__blank"
-          rel="nofollow noopener noreferrer"
-        >
-          https://marketplace.visualstudio.com/vscode
-        </a>
-      </h2>
-      <h2>copy the link of your favorite extentions and submit</h2>
-    </header>
-    <section>
-      <p>Coming soon...</p>
-    </section>
-    <footer className={css.footer}>created by Austin Peterson</footer>
-  </main>
-);
+const Home = ({ test }) => {
+  console.log({ test });
+  return (
+    <main className={css.container}>
+      <header className={css.header}>
+        <h1>♥ VSCode Extentions Poll ♥</h1>
+        <h2>
+          visit{" "}
+          <a
+            href="https://marketplace.visualstudio.com/vscode"
+            target="__blank"
+            rel="nofollow noopener noreferrer"
+          >
+            https://marketplace.visualstudio.com/vscode
+          </a>
+        </h2>
+        <h2>copy the link of your favorite extentions and submit</h2>
+      </header>
+      <section>
+        <p>Coming soon...</p>
+      </section>
+      <footer className={css.footer}>created by Austin Peterson</footer>
+    </main>
+  );
+};
+
+Home.getInitialProps = async ({ req }) => {
+  const { data } = await req;
+  if (data) {
+    return { test: data };
+  }
+  return { test: "error" };
+};
 
 export default Home;
